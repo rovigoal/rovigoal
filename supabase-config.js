fbe98797917bb37ac52134d66dcd8d34d40a9c92
rovigoal <rovigoalsito@gmail.com>
2025-12-31 18:00:30 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.0/+esm'
-
-// Configurazione del tuo database Supabase
-const supabaseUrl = 'https://jgcjhawtmwfsovuegryz.supabase.co'
-const supabaseAnonKey = 'sb_publishable_X6PIatMpCblUcS487cOMfQ_HfP2GpUq'
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-export default supabase;
